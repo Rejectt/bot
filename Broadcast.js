9934861b2056ba9fc02ee6876979ce84c1fb2464
Rejectt <45071435+Rejectt@users.noreply.github.com>
2018-11-25 12:29:51 -0800

    Add files via upload

---
+++ Broadcast.js
@@ -0,0 +1,77 @@
+const Discord = require("discord.js");
+  const kboosh = new Discord.Client();
+    const kbooshtoken = "NTE1OTUxNjAwMTU0NzA1OTYw.Dtx4JQ.atX_Esmp4HJiOUAZ_iae1WTfDgQ"
+      kboosh.on('ready', () => {
+        kboosh.user.setGame(`TEST Broadcast By MohamedS`,'https://www.twitch.tv/TEST-Broadcast');
+          console.log('Hala Mohamed Im Ready To Work !');
+  
+        });
+
+  kboosh.on('message', message => {
+    if (message.content.split(' ')[0] == '!bc')
+       message.guild.members.forEach( member => {
+         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+
+           member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
+                                                      message.delete();
+            
+                                                    });
+            
+                                                  });
+   kboosh.on("message", message => {
+       var prefix = "%";
+ 
+             var args = message.content.substring(prefix.length).split(" ");
+                if (message.content.startsWith(prefix + "b")) {
+                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+                          if (!args[1]) {
+                            
+                                 let embed3 = new Discord.RichEmbed()
+                                     .setDescription(":white_check_mark: | تم ارسال رسالة لا يوجد فيها شيء")
+                                       .setColor("#FF00FF")
+                                          message.channel.sendEmbed(embed3);
+                            
+                                        } else {
+
+                            
+                                           let embed4 = new Discord.RichEmbed()
+                                                            .setDescription(':white_check_mark: | تم ارسال الرساله للجميع ..')
+                                                                .setColor("#99999")
+                               
+                                                                message.channel.sendEmbed(embed4);
+                                                      message.delete();
+                            }
+                          }
+});
+kboosh.login(kbooshtoken);
+client.on('message', message => {
+
+    if (message.content === "g!اقفل") {
+                        if(!message.channel.guild) return message.reply(' This command only for servers');
+
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' ليس لديك صلاحيات');
+           message.channel.overwritePermissions(message.guild.id, {
+         SEND_MESSAGES: false
+
+           }).then(() => {
+               message.reply("تم تقفيل الشات :white_check_mark: ")
+           });
+             }
+//™¦༺♚ƙἶղց|MaS♚༺¦™#7105
+if (message.content === "g!افتح") {
+    if(!message.channel.guild) return message.reply(' This command only for servers');
+
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('ليس لديك صلاحيات');
+           message.channel.overwritePermissions(message.guild.id, {
+         SEND_MESSAGES: true
+
+           }).then(() => {
+               message.reply("تم فتح الشات:white_check_mark:")
+           });
+             }
+
+
+
+});
